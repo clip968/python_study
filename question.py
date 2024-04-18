7858f877f1d7df4ae3dc5dce2114c3c66d78f2cd
clip968 <clip968@gmail.com>
2024-04-18 22:29:05 +0900

    123

--- question.py
+++ question.py
@@ -1,13 +1,46 @@
 class Polynomial():
     def __init__(self):
-        self.arr = list(map(int, input("Input degrees in order").split()))
-        self.length = len(self.arr)
+        self.arr = []
+        self.length = 0
         
     def degree(self, i):
-        return self.length - i
+        return self.length - 1 - i
     
+    def display(self, first):
+        print(first, end='')
+        for i in range(self.length):
+            a = self.degree(i)
+            if (i == self.length - 1):
+                print(self.arr[i])
+            else:
+                print(str(self.arr[i])+"x^"+str(a), end=' + ')
+                
     def read(self):
-        for i in
+        self.arr = list(map(int, input("Input degrees in order : ").split()))
+        self.length = len(self.arr)
+        
+    def add(self, arrB):
+        arrC = Polynomial()
+        
+        if self.length < arrB.length:
+            arrC.length = arrB.length
+            tmp = self.arr
+            self.arr = [0] * (arrB.length - self.length) + self.arr
+            arrC.arr = [0] * arrC.length
+            for i in range(self.length + 1):
+                arrC.arr[i] = arrB.arr[i] + self.arr[i]
+            self.arr = tmp
+            
+        else:
+            arrC.length = self.length
+            arrC.arr = [0] * arrC.length
+            arrB.arr = [0] * (self.length - arrB.length) + arrB.arr
+            tmp = arrB.arr
+            for i in range(arrB.length):
+                arrC.arr[i] = arrB.arr[i] + self.arr[i]
+            arrB.arr = tmp
+        
+        return arrC
             
 if __name__ == '__main__':
     a = Polynomial()
@@ -22,4 +55,4 @@ if __name__ == '__main__':
     a.display("A(x) = ")
     b.display("B(x) = ")
     c.display("C(x) = ")
-    print("C(2) = ", c.eval(2))
+    # print("C(2) = ", c.eval(2))
