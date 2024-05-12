70d865755d9237aad61ef96097779c8d11f23866
clip968 <clip968@gmail.com>
2024-05-13 01:00:18 +0900

    123

--- prime_implicant.py
+++ prime_implicant.py
@@ -1,26 +1,36 @@
 def count_one(num):
-    return bin(num).count('1')
+    result = 0
+    for i in num:
+        if i == '1':
+            result += 1
+    return result
 
 def find_pi(data):
     pi = {}
     for i in data:
         for j in data:
-            diff = int(i, 2) ^ int(j, 2)
-            one_diff = bin(diff).count('1')
-            if one_diff == 1:
-                if count_one(int(i, 2)) not in pi:
-                    pi[count_one(int(i, 2))] = []
-                pi[count_one(int(i, 2))].append((i, j))
+            if i != j:
+                diff = int(i) ^ int(j)
+                one_diff = bin(diff).count('1')
+                if one_diff == 1:
+                    if count_one(i) not in pi:
+                        pi[count_one(i)] = []
+                    pi[count_one(i)].append((i, j))
                     
     return pi
 
-def solution(minterms):
+def Quine_Mccluskey(minterms):
     numberCnt = {}
+    number_binary = []
     for minterm in minterms:
-        b = count_one(minterm)
+        a = bin(minterm)
+        number_binary.append(a[2:].zfill(3))
+    
+    for number in number_binary:
+        b = count_one(number)
         if b not in numberCnt:
             numberCnt[b] = []
-        numberCnt[b].append(bin(minterm)[2:].zfill(3))
+        numberCnt[b].append(number)
     pi = []
     
     for nums in sorted(numberCnt.keys()):
@@ -31,10 +41,10 @@ def solution(minterms):
     result = []
     for group, minterms in sorted(pi):
         for minterm in minterms:
-            result.append('1' * group + minterm.replace('0', '-').replace('1', '1'))
+            result.append(f"{group*'1'}{minterm.replace('0', '-').replace('1', '1')}")
     
     return result
 
 minterms = [3, 6, 0, 1, 2, 5, 6, 7]
-result = solution(minterms)
+result = Quine_Mccluskey(minterms)
 print(result)
